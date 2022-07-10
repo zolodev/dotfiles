eb2dbe2e0bb18bd1fd07ab0eee0c79e29847e5e4
The Jedi <zolodev@jonzzon.nu>
2022-07-10 11:11:13 +0200

    adding .dir_colors

--- setup.sh
+++ setup.sh
@@ -14,8 +14,10 @@ echo $USER
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.vim
 rm -rf $HOME/.bashrc
+rm -rf $HOME/.dir_colors
 
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/bashrc $HOME/.bashrc
+ln -s $PWD/.dir_colors $HOME/.dir_colors
