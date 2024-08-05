78b163f2591d9c2633a75789f1ecf8ed4fdd44ee
Zolo <39656359+zolodev@users.noreply.github.com>
2024-08-05 23:07:27 +0200

    Update start_tmux.sh

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 tmux new-session -d -s main
-tmux split-window -v -p 70           
-
-
-# Calculate sizes based on current window size
-W=$(tmux display -p '#{window_width}')
-L=$(expr $W \* 7 / 10) # 70% for the left pane
-R=$(expr $W \* 3 / 10) # 30% for the right pane
-
-# Resize panes
-tmux resize-pane -t main:0.0 -x $L
-tmux resize-pane -t main:0.1 -x $R
+tmux split-window -v -p 70  # 70% does not seem to work
 
 tmux send-keys -t main:0.0 'btop' C-m
 tmux send-keys -t main:0.1 'tmux clock' C-m
