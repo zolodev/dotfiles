19ce88f3f1b9005e987b7992760209330aff8f43
zolodev <zolo@jonzzon.nu>
2024-08-05 23:16:57 +0200

    Adding comments

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Start a new session named 'main'
 tmux new-session -d -s main
+
+# Split the window
 tmux split-window -v -p 70  # 70% does not seem to work
 
+# Load the first pane with btop
 tmux send-keys -t main:0.0 'btop' C-m
+
+# Load the secon pane with tmux clock
 tmux send-keys -t main:0.1 'tmux clock' C-m
 
+# Attach to the session
 tmux a -t 'main'
